b8734692671d6fb88c2e5da3370ba702c6a8f747
Yu-Hyungi <lyou000000@naver.com>
2025-01-16 23:53:04 +0900

    Add files via upload

--- SignIn.js
+++ SignIn.js
@@ -3,17 +3,22 @@ const IDValue = document.getElementById('IDValue');
 const PWValue = document.getElementById('PWValue');
 const saveButton = document.getElementById('saveButton');
 const outputValue = document.getElementById('outputValue');
-const SignID ={'':''};
 
-
+DuplicationCheck = 0;
 // 아이디 중복 버튼 클릭 이벤트
 dupID.addEventListener('click', () => {
     const ID = IDValue.value;
+    if(JSON.parse(localStorage.getItem('IDPW'))){
+        SignID = JSON.parse(localStorage.getItem('IDPW'));
+    }
+    else{
+        SignID = {'':''};
+    }
 
     if(SignID.hasOwnProperty(ID)){
         DuplicationCheck = 0;
         $('#cor').css('display', 'none');
-        $('#dup').css('display', 'contents');
+        $('#dup').css('display', 'inline');
     }
     else{
         DuplicationCheck = 1;
@@ -23,17 +28,17 @@ dupID.addEventListener('click', () => {
 });
 
 SignInButton.addEventListener('click', () => {
-    if(DuplicationCheck == 1){
+    if((DuplicationCheck == 1)&&(PWValue.value)){
+        const ID = IDValue.value;
+        SignID[ID] = PWValue.value;
+        localStorage.setItem('IDPW', JSON.stringify(SignID));
         alert('회원가입 되었습니다!');
+        location.href="index.html";
     }
-});
-
-
-
-// 페이지 로드 시 로컬 스토리지에서 값 가져오기
-document.addEventListener('DOMContentLoaded', () => {
-    const savedValue = localStorage.getItem('IDComponent');
-    if (savedValue) {
-        outputValue.textContent = savedValue;
+    else if(DuplicationCheck == 0){
+        alert("ID 중복 검사를 해주세요!");
+    }
+    else{
+        alert("정확한 Password를 입력해 주세요!")
     }
 });
--- script.js
+++ script.js
@@ -1,27 +1,18 @@
 // HTML 요소 가져오기
-const inputValue = document.getElementById('inputValue');
-const saveButton = document.getElementById('saveButton');
-const outputValue = document.getElementById('outputValue');
+const inputIDValue = document.getElementById('IDValue');
+const inputPWValue = document.getElementById('PWValue');
 
 // 버튼 클릭 이벤트
-saveButton.addEventListener('click', () => {
-    // 입력 필드의 값 가져오기
-    const value = inputValue.value;
-
-    // 로컬 스토리지에 저장
-    localStorage.setItem('savedValue', value);
-
-    // 출력 영역에 값 표시
-    outputValue.textContent = value;
-
-    // 입력 필드 초기화
-    inputValue.value = '';
-});
-
-// 페이지 로드 시 로컬 스토리지에서 값 가져오기
-document.addEventListener('DOMContentLoaded', () => {
-    const savedValue = localStorage.getItem('savedValue');
-    if (savedValue) {
-        outputValue.textContent = savedValue;
+SignButton.addEventListener('click', () => {
+    const SignID = JSON.parse(localStorage.getItem('IDPW'));
+    
+    if((inputIDValue.value)&&(SignID[inputIDValue.value]==inputPWValue.value)){
+        location.href="main.html";
+    }
+    else{
+        alert("ID, Password를 확인해 주세요!");
     }
 });
+
+
+
